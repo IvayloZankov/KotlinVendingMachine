6fb5c26a8e9c2f9c0f9fffd69a2a561388e86246
Ivailo Zankov <fosents@gmail.com>
2023-04-25 16:44:02 +0300

    update gradle and target

--- app/src/main/java/com/fosents/kotlinvendingmachine/data/local/VendingDatabase.kt
+++ app/src/main/java/com/fosents/kotlinvendingmachine/data/local/VendingDatabase.kt
@@ -7,7 +7,7 @@ import com.fosents.kotlinvendingmachine.data.local.dao.ProductsDao
 import com.fosents.kotlinvendingmachine.model.Coin
 import com.fosents.kotlinvendingmachine.model.Product
 
-@Database(entities = [Product::class, Coin::class], version = 1)
+@Database(entities = [Product::class, Coin::class], version = 1, exportSchema = false)
 abstract class VendingDatabase: RoomDatabase() {
     abstract fun productDao(): ProductsDao
     abstract fun coinDao(): CoinsDao
