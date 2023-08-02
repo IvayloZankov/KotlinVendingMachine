52fbeaaf5013a24896852bfce7b9eba1749542ba
Ivailo Zankov <fosents@gmail.com>
2023-08-02 10:42:47 +0300

    fix

--- app/src/main/java/com/fosents/kotlinvendingmachine/ui/screen/MaintenanceViewModel.kt
+++ app/src/main/java/com/fosents/kotlinvendingmachine/ui/screen/MaintenanceViewModel.kt
@@ -13,25 +13,11 @@ class MaintenanceViewModel @Inject constructor(
     private val dataRepo: DataRepo
 ): ViewModel() {
 
-    private val _noConnection = MutableStateFlow(false)
-    val noConnection: StateFlow<Boolean> = _noConnection
-
     fun initReset(resource: Int) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                if (resource == R.string.maintenance_products_reset) {
-                    dataRepo.resetProducts()
-                } else if (resource == R.string.maintenance_coins_reset) {
-                    dataRepo.resetCoins()
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-                _noConnection.value = true
-    fun initReset(s: String) {
         viewModelScope.request {
-            if (s == getApplication<Application>().getString(R.string.maintenance_products_reset)) {
+            if (resource == R.string.maintenance_products_reset) {
                 dataRepo.resetProducts()
-            } else if (s == getApplication<Application>().getString(R.string.maintenance_coins_reset)) {
+            } else if (resource == R.string.maintenance_coins_reset) {
                 dataRepo.resetCoins()
             }
         }
