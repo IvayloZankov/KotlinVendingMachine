8ba838e6380aecf1a0acf74710fdcf810e610310
Ivaylo Zankov <fosents@gmail.com>
2026-01-20 13:14:59 +0200

    remove accompanist:accompanist-systemuicontroller

--- app/src/main/java/com/fosents/kotlinvendingmachine/ui/theme/Theme.kt
+++ app/src/main/java/com/fosents/kotlinvendingmachine/ui/theme/Theme.kt
@@ -8,9 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple200,
@@ -20,21 +18,11 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple500,
     secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
 fun KotlinVendingMachineTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -47,20 +35,6 @@ fun KotlinVendingMachineTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val systemUiController = rememberSystemUiController()
-    SideEffect {
-        systemUiController.setStatusBarColor(
-            color = if (darkTheme) Teal900 else Teal200
-        )
-    }
-//    val view = LocalView.current
-//    if (!view.isInEditMode) {
-//        SideEffect {
-//            val window = (view.context as Activity).window
-//            window.statusBarColor = colorScheme.primary.toArgb()
-//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-//        }
-//    }
 
     MaterialTheme(
         colorScheme = colorScheme,
