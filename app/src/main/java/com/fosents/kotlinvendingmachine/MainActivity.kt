26edcab5d5dd8420c8b13f2dcf24a5532389ce46
Ivaylo Zankov <fosents@gmail.com>
2026-01-20 15:56:00 +0200

    migrate to compose material ver 1.4

--- app/src/main/java/com/fosents/kotlinvendingmachine/MainActivity.kt
+++ app/src/main/java/com/fosents/kotlinvendingmachine/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material3.LocalRippleConfiguration
+import androidx.compose.material3.RippleConfiguration
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.fosents.kotlinvendingmachine.navigation.SetupNavGraph
 import com.fosents.kotlinvendingmachine.sound.SoundManager
+import com.fosents.kotlinvendingmachine.ui.theme.Gold
 import com.fosents.kotlinvendingmachine.ui.theme.KotlinVendingMachineTheme
-import com.fosents.kotlinvendingmachine.ui.theme.VendingRippleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalAnimationApi
@@ -20,11 +22,20 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var navHostController: NavHostController
 
+    private val myRippleConfiguration = RippleConfiguration(
+        color = Gold, rippleAlpha = RippleAlpha(
+            draggedAlpha = 1f,
+            focusedAlpha = 1f,
+            hoveredAlpha = 1f,
+            pressedAlpha = 0.2f
+        )
+        )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KotlinVendingMachineTheme {
-                CompositionLocalProvider(LocalRippleTheme provides VendingRippleTheme) {
+                CompositionLocalProvider(LocalRippleConfiguration provides myRippleConfiguration) {
                     navHostController = rememberNavController()
                     SetupNavGraph(navController = navHostController)
                 }
